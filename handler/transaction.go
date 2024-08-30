3d21bfd7f05096fe41566317faaedaf5fcce454b
sofyannurrohman <sofyannurrohman1@gmail.com>
2024-08-30 13:22:36 +0700

    Re-Init Transaction

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,14 +1,2 @@
 package handler
 
-import "time"
-
-type Transaction struct {
-	ID         int
-	CampaignID int
-	UserID     int
-	Amount     int
-	Status     string
-	Code       string
-	CreatedAt  time.Time
-	UpdatedAt  time.Time
-}
